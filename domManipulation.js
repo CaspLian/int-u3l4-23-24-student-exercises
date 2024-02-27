7d6ea1ad6ef3d78702d4247ef352ad56b3347b5d
CaspLian <149088248+CaspLian@users.noreply.github.com>
2024-02-27 16:11:27 +0000

    2/27/24

--- domManipulation.js
+++ domManipulation.js
@@ -22,24 +22,25 @@ let image = document.querySelector('.image');
 //   - The `location` paragraph.
 
 contentButton.addEventListener('click', function() {
-
-
-
-
-
-
+  myName.innerHTML = "Caspar Lian";
+  myHobby.innerHTML = "I like to code";
+  myLocation.innerHTML = "I live in New York";
 });
+
 
 // 3. Add an event listener to the `image1Button`
 // 4. When clicked, it should change the source of `image` to another image URL.
-
-
+image1Button.addEventListener('mouseover', function(){
+  image.src ="https://i.imgflip.com/4nswke.gif" ;
+});
 
 
 
 // 5. Add an event listener to the `image2Button`
 // 6. When clicked, it should change the source of `image` to another image URL.
-
+image2Button.addEventListener('mouseover',function(){
+  image.src = "https://img.ifunny.co/images/5344e67f96c706ce92fe5c57063bf08766bd599db0075ff71f6b1273e6792102_1.jpg";
+});
 
 // BONUS
 // 7. Try using different event types for your buttons.
--- script.js
+++ script.js
@@ -3,16 +3,17 @@ let button = document.querySelector("button");
 let img = document.querySelector("img");
 
 // 1. Press the button on the webpage to change it up! Take notice of what happens.
-// - What do you think innerHTML does?
+// - What do you think innerHTML does?/
+//It changes the HTML of the selected element//
 // - What do you think .src does?
-
+//I think it changes the source of the image in the HTML//
 
 // 2. Try the following:
 // - Make it so that the header says your name when the button is clicked.
 // - Change the src of the image to something random.
 button.addEventListener("click", function() {
-    header.innerHTML = "🤯 wow cool neeto";
-    img.src = "https://media4.giphy.com/media/26ufdipQqU2lhNA4g/giphy.gif";
+    header.innerHTML = "Caspar Lian";
+    img.src = "https://img.ifunny.co/images/5344e67f96c706ce92fe5c57063bf08766bd599db0075ff71f6b1273e6792102_1.jpg";
 });
 
 
